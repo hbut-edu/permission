0dde7c6ea6e16cf235cb90fa9019c89327f6e237
liujinhang <liujinhang@hbut.edu.cn>
2022-07-19 10:01:34 +0800

    测试git环境。

--- src/main/java/cn/edu/hbut/manage/permission/PermissionApplication.java
+++ src/main/java/cn/edu/hbut/manage/permission/PermissionApplication.java
@@ -3,6 +3,7 @@ package cn.edu.hbut.manage.permission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PermissionApplication {
 
