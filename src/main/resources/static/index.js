9e22b65264c8013d011965093348b2023139b4f3
liujinhang <liujinhang@hbut.edu.cn>
2022-07-22 21:30:02 +0800

    front-end jquery + semantic-ui style added.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,23 +1,81 @@
-  $(document).ready(function() {
-      
-    $('#btnCheck').click(function (e) {
+let baseURL = "http://localhost:8080/";
 
-      e.preventDefault();
+function dataToHtml(permissions, listPermissions) {
 
-      let id = $('#txtId').val();
+  listPermissions.empty();
 
-      $.ajax({
-        type: "get",
-        url: "http://localhost:8080/users/" + id,
-        dataType: "json",
-        success: function (response) {
-          
-        },
-        error: function (response) {
-          
+  permissions.forEach(permission => {
+
+    let permissionHtml = $("#templatePermission").clone();
+
+    permissionHtml.find(".header")[0].innerText = permission.department.name;
+    permissionHtml.find(".description")[0].innerText = permission.permissionType;
+
+    listPermissions.append(permissionHtml);
+
+  });
+}
+
+$(document).ready(function () {
+
+  $('#btnCheck').click(function (e) {
+    e.preventDefault();
+
+    let id = $('#txtId').val();
+
+    if(id == undefined || id == null || id == NaN || id.trim() == ""){
+      $("#message").text('Please enter your id.');
+      return false;
+    }
+
+    $.ajax({
+      type: "get",
+      url: baseURL + "user/" + id,
+      dataType: "json",
+      success: function (user) {
+
+        dataToHtml(user.permissions, $("#listPermissions"));
+
+      },
+      error: function (response) {
+        $("#message").text(response.responseJSON.message);
+      }
+    });
+
+  });
+
+  $('.ui.form')
+    .form({
+      fields: {
+        id: {
+          identifier: 'txtFormId',
+          rules: [
+            {
+              type: 'empty',
+              prompt: 'Please enter your id.'
+            }
+          ]
         }
-      });
-      
+      },
+      onSuccess: function (e) {
+        e.preventDefault();
+
+        let id = $('#txtFormId').val();
+
+        $.ajax({
+          type: "get",
+          url: baseURL + "user/" + id,
+          dataType: "json",
+          success: function (user) {
+
+            dataToHtml(user.permissions, $("#listFormPermissions"));
+
+          },
+          error: function (response) {
+            console.log(response);
+          }
+        });
+      }
     });
 
 });
